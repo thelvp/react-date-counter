9e3abd554f97edfdedc5c707afa670abe0cd6f0b
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-08-01 17:01:28 +0200

    setup and first additions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './styles.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+export const Counter = () => {
+  const [step, setStep] = useState(1); // increment
+  const [count, setCount] = useState(0); // how many times
+  const today = new Date(); // today's date
+  // TODO: add new date function
+
+  const decreaseStep = () => {
+    setStep((currentStep) => currentStep - 1);
+  };
+
+  const increaseStep = () => {
+    setStep((currentStep) => currentStep + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount((currentCount) => currentCount - 1);
+  };
+
+  const increaseCount = () => {
+    setCount((currentCount) => currentCount + 1);
+  };
+
+  // FIXME: fix everything so it works :')
+  return (
+    <>
+      <div>
+        <button onClick={decreaseStep()}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={increaseStep()}>+</button>
+      </div>
+      <div>
+        <button onClick={decreaseCount()}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={increaseCount()}>+</button>
+      </div>
+      <div>
+        <p>
+          Today it is {today.toLocaleDateString()}. {step * count} days from
+          today it is: {}.
+        </p>
+      </div>
+    </>
+  );
+};
+
+// PSEUDO CODE
+// 1. Add text
+// 2. Add date
+// 3. Add counter function
+// 4. Add step (increment) function
