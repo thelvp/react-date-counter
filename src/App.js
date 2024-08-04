a79b86deb6e7973cadbb66154fb91ee457c9a369
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-08-04 10:53:50 +0200

    basic button functionality

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import { useState } from 'react';
 import './styles.css';
 
 export default function App() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(1);
+
+  const handleMinusSteps = () => {
+    setStep((c) => c - 1);
+  };
+
+  const handlePlusSteps = () => {
+    setStep((c) => c + 1);
+  };
+
+  const handleMinusCount = () => {
+    setCount((c) => c - 1);
+  };
+
+  const handlePlusCount = () => {
+    setCount((c) => c + 1);
+  };
+
   return (
     <div className='App'>
       <div>
         <div className='controls'>
-          <button>-</button>
-          <p>Step: {}</p>
-          <button>+</button>
+          <button onClick={handleMinusSteps}>-</button>
+          <p>Step: {step}</p>
+          <button onClick={handlePlusSteps}>+</button>
         </div>
         <div className='controls'>
-          <button>-</button>
-          <p>Count: {}</p>
-          <button>+</button>
+          <button onClick={handleMinusCount}>-</button>
+          <p>Multiplied by: {count}</p>
+          <button onClick={handlePlusCount}>+</button>
         </div>
       </div>
       <div>
